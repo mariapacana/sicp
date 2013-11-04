0f2a013bb39e305af9585f6d0872f19a0983cce7
Maria Pacana <maria.pacana@gmail.com>
2013-11-04 14:48:23 -0500

    Finished 1.23

--- 1/1_23.scm
+++ 1/1_23.scm
@@ -3,12 +3,18 @@
 (define (square x)
   * x x)
 
+(define (next n)
+  (if (= n 2)
+      3
+      (+ n 2)))
+
 (define (smallest-divisor n)
   (find-divisor n 2))
+
 (define (find-divisor n test-divisor)
   (cond ((> (square test-divisor) n) n)
         ((divides? test-divisor n) test-divisor)
-        (else (find-divisor n (+ test-divisor 1)))))
+        (else (find-divisor n (next test-divisor)))))
 (define (divides? a b)
   (= (remainder b a) 0))
 
@@ -27,15 +33,15 @@
   (display " *** ")
   (display elapsed-time))
 
-(define (search-for-primes start numprimes)
-  (timed-prime-test start)
-  (if (= numprimes 0) 
-      (display " we're done")
-      (if (prime? start) 
-          (search-for-primes (+ start 1) (- numprimes 1))
-          (search-for-primes (+ start 1) numprimes))))
-
-(search-for-primes 1000 3); 1009(197), 1013(192), 1019(192)
-;(search-for-primes 10000 3); 10007(2161), 10009(2026), 10037(1977)
-;(search-for-primes 100000 3); 100003(23325), 100019(19874), 100043(19962)
-;(search-for-primes 1000000 3); 1000003(190170), 1000033(213399), 1000037(259015)
+(timed-prime-test 1009); 197 (329)
+(timed-prime-test 1013); 192 (128)
+(timed-prime-test 1019); 192 (127)
+(timed-prime-test 10007); 2161 (1219)
+(timed-prime-test 10009); 2026 (1285)
+(timed-prime-test 10037); 1977 (1264)
+(timed-prime-test 100003); 23325 (12459)
+(timed-prime-test 100019); 19874 (12392)
+(timed-prime-test 100043); 19962 (11991)
+(timed-prime-test 1000003); 190170 (126464)
+(timed-prime-test 1000033); 213399 (120695)
+(timed-prime-test 1000037); 259015 (120761)
