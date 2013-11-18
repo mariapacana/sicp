8c4c79ee42dfb65af157355dc8c2b833a173c83a
Maria Pacana <maria.pacana@gmail.com>
2013-11-18 17:45:51 -0500

    Finished 2.1

---
+++ 2/2_1.scm
@@ -0,0 +1,39 @@
+#lang racket
+
+; arithmetic operations on rational numbers
+
+(define (add-rat x y)
+  (make-rat (+ (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+(define (sub-rat x y)
+  (make-rat (- (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+(define (mul-rat x y)
+  (make-rat (* (numer x) (numer y))
+            (* (denom x) (denom y))))
+(define (div-rat x y)
+  (make-rat (* (numer x) (denom y))
+            (* (denom x) (numer y))))
+(define (equal-rat? x y)
+  (= (* (numer x) (denom y))
+     (* (numer y) (denom x))))
+
+; Defining a numerator and a denominator
+
+(define (numer x) (car x))
+
+(define (denom x) (cdr x))
+
+(define (print-rat x)
+  (newline)
+  (display (numer x))
+  (display "/")
+  (display (denom x)))
+
+(define (make-rat n d)
+  (let ((g (gcd n d)))
+  (if (< (* n d) 0)
+      (cons (- (/ n g)) (abs (/ d g)))
+      (cons (/ n g) (abs (/ d g))))))
